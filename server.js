4d5d4d3921a26ef3aa3da0c096361c8fbd6aa4e2
Matt Kilcup <nevermindthelabel@gmail.com>
2019-06-16 07:37:32 -0700

    created basic server

---
+++ server.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const routes = require("./routes");
+const db = require("./db");
+const app = express();
+const PORT = process.env.PORT || 3001;
+
+app.use(
+  express.urlencoded({
+    extended: true
+  })
+);
+app.use(express.json());
+if (process.env.NODE_ENV === "production") {
+  app.use(express.static("client/build"));
+}
+
+app.use(routes);
+
+app.listen(PORT, () => {
+  console.log(
+    `🌎  ==> API Server now listening on PORT http://localhost:${PORT}`
+  );
+});
