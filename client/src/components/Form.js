7143c5a9505b3aac701e5e816c9be00d3a2c2a05
Matt Kilcup <nevermindthelabel@gmail.com>
2019-08-24 18:13:10 -0700

    moved state logic into component for now

--- client/src/components/Form.js
+++ client/src/components/Form.js
@@ -1,21 +1,63 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Form, Button } from 'react-bootstrap';
+import API from '../utils/api';
+import api from '../utils/api';
 
 const Forms = () => {
+
+   const [formData, setFormData] = useState({
+     address: '',
+     rent: ''
+   });
+
+   const handleChange = event => {
+     const { name, value } = event.target;
+     setFormData({ ...formData, [name]: value });
+   };
+
+   console.log(formData);
+
+   const { address, rent } = formData;
+
+  //  const handleSubmit = event => {
+  //    event.preventDefault();
+  //    console.log('submitted');
+  //  };
+
   return (
     <div>
       <Form>
         <h2 className="text-center text-danger">Enter a new house</h2>
-        <Form.Group controlId="formBasicEmail">
+        <Form.Group>
           <Form.Label>Property address</Form.Label>
-          <Form.Control type="text" placeholder="Enter address" />
+          <Form.Control
+            type="text"
+            name="address"
+            onChange={handleChange}
+            value={address}
+            placeholder="Enter address"
+          />
         </Form.Group>
 
-        <Form.Group controlId="formBasicPassword">
+        <Form.Group>
           <Form.Label>Enter monthly Rent</Form.Label>
-          <Form.Control type="text" placeholder="Monthly Rent" />
+          <Form.Control
+            type="text"
+            name="rent"
+            onChange={handleChange}
+            value={rent}
+            placeholder="Monthly Rent"
+          />
         </Form.Group>
-        <Button variant="primary" type="submit">
+        <Button
+          variant="primary"
+          type="submit"
+          onClick={e => {
+            e.preventDefault();
+            console.log('clicked');
+            api.newHouse(formData);
+          }}
+        >
           Submit
         </Button>
       </Form>
