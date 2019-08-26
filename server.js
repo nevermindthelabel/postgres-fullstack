156d8cb5a91aae486e64ba426b1eb8efd0ab22f8
Matt Kilcup <nevermindthelabel@gmail.com>
2019-08-25 21:35:23 -0700

    added headers

--- server.js
+++ server.js
@@ -1,7 +1,13 @@
-const express = require("express");
-const routes = require("./routes");
+const express = require('express');
+const routes = require('./routes');
 const app = express();
 const PORT = process.env.PORT || 3001;
+
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*');
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept');
+  next();
+});
 
 app.use(
   express.urlencoded({
@@ -9,14 +15,12 @@ app.use(
   })
 );
 app.use(express.json());
-if (process.env.NODE_ENV === "production") {
-  app.use(express.static("client/build"));
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 }
 
 app.use(routes);
 
 app.listen(PORT, () => {
-  console.log(
-    `🌎  ==> API Server now listening on PORT http://localhost:${PORT}`
-  );
+  console.log(`🌎  ==> API Server now listening on PORT http://localhost:${PORT}`);
 });
