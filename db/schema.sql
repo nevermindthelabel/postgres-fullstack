81d36d2f7f527ceb1e442003208430f895e64970
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-13 21:57:55 -0700

    updated name for pool

--- db/schema.sql
+++ db/schema.sql
@@ -1,4 +1,4 @@
-create table house (ID SERIAL PRIMARY KEY, address VARCHAR(30), price money, square_feet integer, num_bed integer, num_bath integer, garage boolean, num_stories integer, pool boolean)
+create table house (ID SERIAL PRIMARY KEY, address VARCHAR(30), price money, square_feet integer, num_bed integer, num_bath integer, garage boolean, num_stories integer, swimming_pool boolean)
 INSERT INTO house (address, price, square_feet, num_bed, num_bath,garage, num_stories, swimming_pool)
 VALUES('123 fake street',
        1500,
