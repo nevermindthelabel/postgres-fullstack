e8b7cf40866195d2ebaf74893fda2ad5c6a50b97
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-13 21:56:49 -0700

    added insert statement

--- db/schema.sql
+++ db/schema.sql
@@ -1,1 +1,11 @@
 create table house (ID SERIAL PRIMARY KEY, address VARCHAR(30), price money, square_feet integer, num_bed integer, num_bath integer, garage boolean, num_stories integer, pool boolean)
+INSERT INTO house (address, price, square_feet, num_bed, num_bath,garage, num_stories, swimming_pool)
+VALUES('123 fake street',
+       1500,
+       1500,
+       3,
+       2,
+       true,
+       1,
+       true);
+
