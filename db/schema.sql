1908006bd55abf9b575636b9fce2f7c9c4541da4
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-13 21:29:16 -0700

    added basic schema

---
+++ db/schema.sql
@@ -0,0 +1,1 @@
+create table house (ID SERIAL PRIMARY KEY, address VARCHAR(30), price money, square_feet integer, num_bed integer, num_bath integer, garage boolean, num_stories integer, pool boolean)
