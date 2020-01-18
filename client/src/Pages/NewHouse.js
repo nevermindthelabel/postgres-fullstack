2fd0649c7a00f55486c5f59740e743654e542155
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-17 22:05:46 -0700

    moved form page

---
+++ client/src/Pages/NewHouse.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import Form from '../components/Form';
+import { Container } from 'react-bootstrap';
+
+const NewHouse = () => {
+  return (
+    <Container>
+      <Form />
+    </Container>
+  )
+};
+
+export default NewHouse;
