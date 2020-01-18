25384a68686ef4d1e0195ac11924cf062b846fa8
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-17 17:11:24 -0700

    created route to add house to databasae

--- routes/api/homes.js
+++ routes/api/homes.js
@@ -3,8 +3,9 @@ const db = require("../../db");
 
 const router = new Router()
 
-router.route("/").get(db.getHouses);
+router.route('/').get(db.getHouses);
 router.route('/:id').get(db.getHousesById);
+router.route('/new').post(db.createHouse)
 router.route('/remove/:id').delete(db.removeHouse);
 
 module.exports = router;
