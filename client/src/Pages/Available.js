fad2ba9d808f7d9b7fea2a4f876a151b3a0467a6
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-17 18:02:40 -0700

    removed unnecessary import and cleaned up initial styling

--- client/src/Pages/Available.js
+++ client/src/Pages/Available.js
@@ -1,6 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { Container } from 'react-bootstrap';
-import api from '../utils/api';
+import { Container, Card } from 'react-bootstrap';
 import axios from 'axios';
 
 const Available = () => {
@@ -9,16 +8,20 @@ const Available = () => {
 
   useEffect(() => {
     axios.get('/api/houses').then(data => {
-      console.log(data)
+      console.log(data.data);
+      setAvilable(data.data);
+      setLoading(false);
     })
   }, []);
 
   return (
-    <div>
       <Container>
         <h2>things and stuff</h2>
+        {
+          loading ? <h2>loading...</h2> :
+available.map(house => (<Card key={house.id}><h2>{house.address}</h2><p>{house.price}</p></Card>))
+        }
       </Container>
-    </div>
   )
 };
 
