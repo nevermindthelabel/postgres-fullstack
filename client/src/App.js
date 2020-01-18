c7c0d8da257937505df9c20f4ff895b7aa6cf309
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-17 22:06:00 -0700

    added new page

--- client/src/App.js
+++ client/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
 import Home from './Pages/Home';
 import Available from './Pages/Available';
+import NewHouse from './Pages/NewHouse';
 import './App.css';
 
 function App() {
@@ -11,6 +12,7 @@ function App() {
         <Switch>
           <Route exact path='/' component={Home} />
           <Route exact path='/available' component={Available} />
+          <Route exact path='/new' component={NewHouse} />
         </Switch>
       </Router>
     </div>
