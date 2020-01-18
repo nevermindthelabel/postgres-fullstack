dc1c642fff8e9aa3daa05e7b7bb5e9ee9b87d30a
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-17 22:06:22 -0700

    moved form to its own page

--- client/src/Pages/Home.js
+++ client/src/Pages/Home.js
@@ -1,39 +1,12 @@
 import React, { } from 'react';
 import { Container } from 'react-bootstrap';
 import Form from '../components/Form';
-// import api from '../utils/api';
 
 const Home = () => {
-  // const [state, setGetAllHomes] = useState('')
-
-  // const getHomes = async () => {
-  //   await api.houses.getHouses().then(res => setGetAllHomes(...state, setGetAllHomes))
-  // };
-
-//  useEffect(() => {
-//     getHomes()
-//   }, []);
-
-  // const [formData, setFormData] = useState({
-  //   address: '',
-  //   price: ''
-  // });
-
-  // const handleChange = event => {
-  //   const { name, value } = event.target;
-  //   setFormData({ ...formData, [name]: value });
-  // };
-
-  // console.log(formData);
-
-  // const handleSubmit = event => {
-  //   event.preventDefault();
-  //   console.log('submitted');
-  // };
 
   return (
     <Container>
-      <Form />
+     <h1>Welcome to the Home page</h1>
     </Container>
   );
 };
