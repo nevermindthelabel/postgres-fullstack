127ee7392529bca4cadae22cbb307bb0baa34d0d
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-17 18:32:06 -0700

    added additonal data

--- client/src/Pages/Available.js
+++ client/src/Pages/Available.js
@@ -19,10 +19,12 @@ const Available = () => {
       <h2>Available Homes</h2>
       {
         loading ? <h2>loading...</h2> :
-          available.map(house => (<Card style={{maxWidth:'50%', margin:'auto'}} key={house.id}>
+          available.map(house => (<Card style={{ maxWidth: '50%', margin: 'auto' }} key={house.id}>
             <h2>{house.address} in {house.city}</h2>
-            <h3>{house.price}</h3>
-            <h4>{house.square_feet}</h4>
+            <h3>Price: {house.price}</h3>
+            <h4>Square Feet: {house.square_feet}</h4>
+            <h4>Bedrooms: {house.num_bed}</h4>
+            <h4>Bathrooms: {house.num_bath}</h4>
           </Card>))
       }
     </Container>
