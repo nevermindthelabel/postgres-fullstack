f1747877057e4d51567c2fa863d4839ad21823ba
Matt Kilcup <nevermindthelabel@gmail.com>
2020-01-19 13:45:49 -0700

    added remove route

--- client/src/utils/api.js
+++ client/src/utils/api.js
@@ -3,6 +3,7 @@ import axios from 'axios';
 export default {
   houses: {
     getHouses: () => axios.get('/api/houses'),
-    newHouse: (data) => axios.post('/api/houses/new', data)
+    newHouse: (data) => axios.post('/api/houses/new', data),
+    removeHouse: (id) => axios.delete(`/remove/${id}`)
   }
 }
